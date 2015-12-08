4971a79b1b608cc7bee4caae9187ebad3248791c
Satish Talluri <satish@neptune.io>
2015-12-08 14:52:55 -0800

    Tar unzip to neptune agent home

--- scripts/linux/install_neptune_agent_linux.sh
+++ scripts/linux/install_neptune_agent_linux.sh
@@ -107,7 +107,7 @@ sleep 2
 echo "Fetching the latest version of neptune agent and daemon"
 sudo $DOWNLOAD_CMD $NEPTUNE_AGENT_HOME/${NEPTUNE_AGENT}-${PLATFORM}-${ARCH}.tar.gz $NEPTUNE_AGENT_URL/downloads/${NEPTUNE_AGENT}-${PLATFORM}-${ARCH}.tar.gz
 sudo $DOWNLOAD_CMD $NEPTUNE_AGENT_HOME/$NEPTUNE_AGENT_DAEMON $NEPTUNE_AGENT_URL/scripts/$PLATFORM/$NEPTUNE_AGENT_DAEMON
-sudo tar -zxf $NEPTUNE_AGENT_HOME/${NEPTUNE_AGENT}-${PLATFORM}-${ARCH}.tar.gz
+sudo tar -zxf $NEPTUNE_AGENT_HOME/${NEPTUNE_AGENT}-${PLATFORM}-${ARCH}.tar.gz -C $NEPTUNE_AGENT_HOME
 
 # Update repo URL in the daemon to enable agent updates
 sudo sed -i "s|AGENT_USER_HERE|$NEPTUNE_AGENT_USER|" $NEPTUNE_AGENT_HOME/$NEPTUNE_AGENT_DAEMON
